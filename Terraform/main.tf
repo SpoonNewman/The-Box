653b612232ca6a67fe29af5f3ae5ab40f1f3ccba
SpoonNewman <96314739+SpoonNewman@users.noreply.github.com>
2023-08-17 17:03:21 -0400

    We made terraform delete stuff

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -81,10 +81,10 @@ resource "aws_kms_key" "bucket_key" {
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 
